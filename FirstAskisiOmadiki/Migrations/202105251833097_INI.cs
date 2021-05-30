08a29f6f9e13b4d1389ae835432d9d766739e993
Konstantina <konstantinalakoumenta@gmail.com>
2021-05-30 17:42:37 +0300

    Added files
    Mpolika pragmata

---
+++ FirstAskisiOmadiki/Migrations/202105251833097_INI.cs
@@ -0,0 +1,29 @@
+﻿namespace FirstAskisiOmadiki.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class INI : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        IsExpired = c.Boolean(nullable: false),
+                        DateModified = c.DateTime(nullable: false),
+                        Price = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
---
+++ FirstAskisiOmadiki/Migrations/Configuration.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using FirstAskisiOmadiki.Models;
+
+namespace FirstAskisiOmadiki.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FirstAskisiOmadiki.Data.MyDatabase>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FirstAskisiOmadiki.Data.MyDatabase context)
+        {
+            var p1 = new Product()
+            { Title = "Patatakia", IsExpired = true, DateModified = new DateTime(2021, 5, 25, 21, 15, 0) , Price = 2};
+            var p2 = new Product()
+            { Title = "Sokolates", IsExpired = false, DateModified = new DateTime(2021, 5, 25, 21, 15, 0) ,Price = 3};
+            var p3 = new Product()
+            { Title = "Fistikia", IsExpired = true, DateModified = new DateTime(2021, 5, 25, 21, 15, 0),Price = 5};
+            var p4 = new Product()
+            { Title = "Whiskey", IsExpired = true, DateModified = new DateTime(2021, 5, 25, 21, 15, 0) ,Price = 70};
+
+            var products = new List<Product>(){p1,p2,p3,p4};
+
+            foreach (var product in products)
+            {
+                context.ProductsDbSet.AddOrUpdate(p=>new{p.Title,p.Price},product);
+            }
+
+        }
+    }
+}
